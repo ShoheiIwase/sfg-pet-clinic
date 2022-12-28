021fccb08667b9ee3eec066c8292109d684842f7
Shohei Iwase <shohei@ShoheinoMBP.AirPort>
2022-12-28 19:48:25 +0900

    initial commit

---
+++ src/test/java/iwase/shohei/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package iwase.shohei.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
